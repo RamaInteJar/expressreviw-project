9f50354f71f291784eac4c8271caa7543928f231
pentiumKing <ramahawai87@gmail.com>
2023-07-24 09:31:33 -0400

    updated the show.ejs file

--- server.js
+++ server.js
@@ -1,38 +1,54 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const stocks = require('./models/stocks.js')
+const stocks = require("./models/stocks.js");
+//Makes a layout to wrap our pages
+const expressEjsLayouts = require("express-ejs-layouts");
 
 // console.log(stocks);
-app.set('view engine', 'ejs')
 //MIDDLEWARE
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
 
 //ROUTES
 //INDUCES => INDEX, NEW, DELETE, UPDATE, CREATE, EDIT, SHOW
 //MVC => Model, View, Controller
 //Model => some data definitions, / database to grab and do CRUD with
-// View => ejs or whatever the front-end sees 
+// View => ejs or whatever the front-end sees
 //Controller => cont
 
 //INDEX ROUTES
-app.get('/', (req, res)=>{
-    // res.send(`J'ai besoin d'argent et j'en ai besoin maintenant`)
-    res.render('index', {stocks})
-})
+app.get("/", (req, res) => {
+  // res.send(`J'ai besoin d'argent et j'en ai besoin maintenant`)
+  res.render("index", { stocks });
+});
+
+app.get("/stocks", (req, res) => {
+  res.send(stocks);
+});
 
 
-app.get('/stocks', (req, res)=>{
-    res.send(stocks)
-})
-app.get('/stocks/:id', (req, res)=>{
-    res.render('show')
-})
+app.get("/stocks/:name", (req, res) => {
+  const name = req.params.name;
+  //with find()
+  //let stock = stocks.find((s) => s.name.toLowerCase() === name.toLowerCase());
+
+  //with for loop
+  let stock;
+  for(let i = 0; i < stocks.length; i++){
+    if(stocks[i].name.toLowerCase() === name.toLowerCase()){
+        stock = stocks[i]
+    }
+  }
+ console.log(stock);
+  res.render("show.ejs", {stock});
+});
 
 //CATCH ALL ROUTES ERRORS
-app.get('/*', (req, res)=>{
-    res.send("404, Page not found")
-})
+app.get("/*", (req, res) => {
+  res.send("404, Page not found");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`PORT:  ${PORT} in use do your person!`);
-})
+app.listen(PORT, () => {
+  console.log(`PORT:  ${PORT} in use do your person!`);
+});
