2c0c76a3d87ef1477411fcb7791a768b115f3a7a
pentiumKing <ramahawai87@gmail.com>
2023-07-23 22:21:16 -0400

    Added a  json data

---
+++ models/stocks.js
@@ -0,0 +1,34 @@
+const stocks = [
+    {
+      symbol: "TSLA",
+      name: "Tesla",
+      closingPrice: 150,
+      oppeningPrice: 200,
+      purchasePrice: 100,
+      day: "2023-07-22",
+    },
+    {
+      symbol: "APPL",
+      name: "Apple",
+      closingPrice: 120,
+      oppeningPrice: 90,
+      purchasePrice: 80,
+      day: "2023-07-22",
+    },
+    {
+      symbol: "NVDA",
+      name: "NVidia",
+      closingPrice: 400,
+      oppeningPrice: 300,
+      purchasePrice: 200,
+      day: "2023-07-22",
+    },
+    {
+      symbol: "BABA",
+      name: "Alibaba",
+      closingPrice: 300,
+      oppeningPrice: 250,
+      purchasePrice: 200,
+      day: "2023-07-22",
+    },
+  ];
