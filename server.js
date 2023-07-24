3d8cbcef85d05789a58c650291e1f08f05dfb5fa
pentiumKing <ramahawai87@gmail.com>
2023-07-23 23:14:01 -0400

    added models and views folders together with theirs associated files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const PORT = 3000;
+const stocks = require('./models/stocks.js')
 
-
+// console.log(stocks);
+app.set('view engine', 'ejs')
 //MIDDLEWARE
 
 //ROUTES
@@ -12,8 +14,18 @@ const PORT = 3000;
 // View => ejs or whatever the front-end sees 
 //Controller => cont
 
+//INDEX ROUTES
 app.get('/', (req, res)=>{
-    res.send(`J'ai besoin d'argent et j'en ai besoin maintenant`)
+    // res.send(`J'ai besoin d'argent et j'en ai besoin maintenant`)
+    res.render('index')
+})
+
+
+app.get('/stocks', (req, res)=>{
+    res.send(stocks)
+})
+app.get('/stocks/:id', (req, res)=>{
+    res.render('show')
 })
 
 //CATCH ALL ROUTES ERRORS
